761dfaf813383aa36302f908ce0a84189c5b2f45
vgnz93hs <ilce6400.mail@gmail.com>
2024-02-29 22:53:00 +0900

    Test

--- files/js/header.js
+++ files/js/header.js
@@ -2,7 +2,7 @@ document.getElementById("header").innerHTML = `
 <header class="header-large">
 <nav>
     <ul class="header-large-ul">
-        <li class="header-menu"><a href="/home//vgnz93hs.github.io/home/" class="header-menu-button"><span class="header-menu-button-text"><img src="/home//vgnz93hs.github.io/home/files/icons/icon.svg" width="20px" class="header-menu-button-image"></span><span class="header-menu-button-text">vgnz93hs</span></a></li>
+        <li class="header-menu"><a href="/home//vgnz93hs.github.io/home/" class="header-menu-button"><span class="header-menu-button-text"><img src="//vgnz93hs.github.io/home/files/icons/icon.svg" width="20px" class="header-menu-button-image"></span><span class="header-menu-button-text">vgnz93hs</span></a></li>
         <li class="header-menu"><a href="/home/about/" class="header-menu-button"><span>vgnz93hsについて</span></a></li>
         <li class="header-menu"><a href="/home/works/" class="header-menu-button"><span>作品</span></a></li>
         <li class="header-menu"><a href="/home/explanation/" class="header-menu-button"><span>解説</span></a></li>
@@ -14,7 +14,7 @@ document.getElementById("header").innerHTML = `
 <header class="header-small">
 <nav>
     <ul class="header-small-ul">
-        <li class="header-menu"><a href="http://ilce6400.starfree.jp" class="header-menu-button"><span class="header-menu-button-text"><img src="/home//vgnz93hs.github.io/home/files/icons/icon.svg" width="20px" class="header-menu-button-image"></span><span class="header-menu-button-text">vgnz93hs</span></a></li>
+        <li class="header-menu"><a href="//vgnz93hs.github.io/home/" class="header-menu-button"><span class="header-menu-button-text"><img src="//vgnz93hs.github.io/home/files/icons/icon.svg" width="20px" class="header-menu-button-image"></span><span class="header-menu-button-text">vgnz93hs</span></a></li>
         <li class="header-menu"><a href="/home/pages/" class="header-menu-button"><span>ページ</span></a></li>
     </ul>
 </nav>
